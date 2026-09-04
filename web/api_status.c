623d8ae3ceecbe2936731d09ddfff6045ade76f7
f123-b <1091521296@qq.com>
2026-09-04 12:09:41 +0800

    fix: read CAN traffic counters from SocketCAN HAL

--- web/api_status.c
+++ web/api_status.c
@@ -17,6 +17,7 @@
 #include "../services/mqtt_client.h"
 #include "../services/data_bus.h"
 #include "../services/modbus_master.h"
+#include "../hal/can_socket.h"
 #include "../ota_manager.h"
 #include "../ntp_sync.h"
 #include <stdio.h>
@@ -27,10 +28,6 @@
 extern void web_send_response(int client_fd, int status,
                               const char *content_type,
                               const char *body, size_t body_len);
-
-/* CAN 发送计数仍由主程序测试发送路径提供；Modbus 计数由服务模块维护 */
-extern int can_tx_cnt;
-extern int can_rx_cnt;
 
 void api_status_handle(int client_fd)
 {
@@ -120,7 +117,7 @@ void api_status_handle(int client_fd)
         /* can */
         can_count > 0 ? "true" : "false",
         can_count,
-        can_tx_cnt, can_rx_cnt,
+        can_get_tx_count(), can_get_rx_count(),
         /* ota */
         ota_str, ota_get_progress(),
         ver, OTA_DEFAULT_SERVER,
